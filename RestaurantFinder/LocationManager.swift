b15fccd5b476ba39bddda8e4c8b9d179d0656bcf
Kevin VanEvery <kvanever@gmail.com>
2016-07-31 13:52:31 -0700

    add coordinate extension to location manager

--- RestaurantFinder/LocationManager.swift
+++ RestaurantFinder/LocationManager.swift
@@ -9,9 +9,17 @@
 import Foundation
 import CoreLocation
 
+extension Coordinate {
+    init(location: CLLocation) {
+        latitude = location.coordinate.latitude
+        longitude = location.coordinate.latitude
+    }
+}
+
 final class LocationManager: NSObject, CLLocationManagerDelegate {
     let manager = CLLocationManager()
     
+    var onLocationFix: (Coordinate -> Void)?
     override init() {
         super.init()
         manager.delegate = self
@@ -20,6 +28,28 @@ final class LocationManager: NSObject, CLLocationManagerDelegate {
     func getPermission() {
         if CLLocationManager.authorizationStatus() == .NotDetermined {
             manager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    // MARK: CLLocationManagerDelegate
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == .AuthorizedWhenInUse {
+            manager.requestLocation()
+        }
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print(error.description)
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else { return }
+        
+        let coordinate = Coordinate(location: location)
+        if let onLocationFix = onLocationFix {
+            onLocationFix(coordinate)
         }
     }
 }
